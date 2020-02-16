7ad986a556536c3a76b4ed286ca66f713cf8fa09
MechJM <matosjoaops@protonmail.com>
2020-02-16 21:56:50 +0000

    Exercise 4 is done except for that weird bug

--- Aula 1/hello.c
+++ Aula 1/hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*
 //Exs.2-3
@@ -21,14 +22,20 @@ int main(int argc, char* argv[])
 
 	char nome[MAX_NAME_LEN];
 
+	int n=atoi(argv[1]);
 
-	for	(int i=1;i<argc;i++)
+	
+
+	for	(int i=2;i<argc;i++)
 	{
 		sprintf(nome+strlen(nome),"%s",argv[i]);
 		sprintf(nome+strlen(nome)," ");
 	}
 
-	printf("Hello %s !\n",nome);
+	for (int i=0;i<n;i++)
+	{
+		printf("Hello %s !\n",nome);
+	}
 
 	return 0;
 }
