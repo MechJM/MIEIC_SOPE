8a3790c38d7b86f9177304932d25983df097b581
MechJM <matosjoaops@protonmail.com>
2020-02-17 20:16:41 +0000

    Ex.8 is mostly done, just requires some debugging

--- Aula 1/exec_time.c
+++ Aula 1/exec_time.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/times.h>
 
 
 int main(int argc,char *argv[])
@@ -23,6 +24,17 @@ int main(int argc,char *argv[])
         sleep(1);
     }
 
+
+    time_t raw_time = time(0);
+    struct tm *current_time=localtime(&raw_time);
+
+    int ticks_sec = sysconf(_SC_CLK_TCK);
+    struct tms *sys_time;
+    times(sys_time);
+
+    printf("Tempo real: %d:%d\n",current_time->tm_hour,current_time->tm_min);
+    printf("Tempo CPU(user): %ld segundos\n",sys_time->tms_utime/ticks_sec);
+    printf("Tempo CPU(system): %ld segundos\n",sys_time->tms_stime/ticks_sec);
 
     return 0;
 }
