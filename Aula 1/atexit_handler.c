dc8b5eb1895bc6ca202abc73f58c63bd10b5ddbb
MechJM <matosjoaops@protonmail.com>
2020-02-17 17:40:15 +0000

    Ex.7 is done

--- Aula 1/atexit_handler.c
+++ Aula 1/atexit_handler.c
@@ -4,12 +4,13 @@
 void atexit_handler1(void)
 {
     printf("Executing exit handler 1\n");
+    //exit(0);
 }
 
 void atexit_handler2(void)
 {
     printf("Executing exit handler 2\n");
-    exit(0);
+    //exit(0);
 }
 
 int main(void)
@@ -17,6 +18,6 @@ int main(void)
     //abort();
     printf("Main done!\n");
     atexit(atexit_handler2);
-
+    atexit(atexit_handler1);
     exit(0);
 }
