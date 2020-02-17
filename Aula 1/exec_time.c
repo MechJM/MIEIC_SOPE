e41afc2d2a8e6cbe78beefffce1a3cbd07f9dc3e
MechJM <matosjoaops@protonmail.com>
2020-02-17 19:42:39 +0000

    Ex.8 a) is done

--- Aula 1/exec_time.c
+++ Aula 1/exec_time.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 
-int main(int argc,char **argv)
+int main(int argc,char *argv[])
 {
     int num1,num2,rand_num,i=1;
 
     num1=atoi(argv[1]);
     num2=atoi(argv[2]);
-
 
     srand(time(NULL));
     rand_num=rand()%num1;
@@ -17,13 +17,12 @@ int main(int argc,char **argv)
 
     while(rand_num!=num2)
     {
-        srand(time(NULL));
+        i++;
         rand_num=rand()%num1;
         printf("%d:%d\n",i,rand_num);
         sleep(1);
     }
 
-    printf("Sai do loop\n");
 
     return 0;
 }
