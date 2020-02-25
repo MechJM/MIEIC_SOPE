9b37c1cd09fac59427b6382307df18a73ab2dd62
MechJM <matosjoaops@protonmail.com>
2020-02-25 12:34:27 +0000

    Ex.4 a) is no longer broken

--- Aula 2/text_file.c
+++ Aula 2/text_file.c
@@ -10,21 +10,23 @@
 
 int main(int argc,char* argv[])
 {
-    char students[MAX_STUD][MAX_STRLEN],ch,answer[3]="";
+    char students[MAX_STUD][MAX_STRLEN],ch,answer[4]="";
     int i=0,flag=1;
 
     while(flag)
     {   
         write(STDOUT_FILENO,"Indique o nome e classificacao do aluno:\n",41);
-        for (int i2=0;i2<MAX_STRLEN;i2++)
+        int i2;
+        for (i2 = 0; i2 < MAX_STRLEN; i2++)
         {
-            if (ch == '\n') break;
             if (read(STDIN_FILENO,&ch,1) != 1) break;
+            if (ch == '\n') break;
             students[i][i2]=ch;
         }
+        students[i][i2]=0;
         write(STDOUT_FILENO,"Pretende indicar mais estudantes?(sim/nao)\n",43);
-        read(STDIN_FILENO,&answer,3);
-        if (strcmp(answer,"sim")!=0) flag=0;
+        read(STDIN_FILENO,&answer,4);
+        if (strcmp(answer,"sim\n")!=0) flag=0;
         i++;
     }
 
@@ -33,6 +35,7 @@ int main(int argc,char* argv[])
     for (int i3 = 0; i3 < i; i3++)
     {
         write(file,students[i3],strlen(students[i3]));
+        if (i3 != (i-1)) write(file,"\n",1);
     }
 
     close(file);
