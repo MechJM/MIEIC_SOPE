0a73d151b89cbd5525531c1e1b0a89a90377c4ab
MechJM <matosjoaops@protonmail.com>
2020-02-27 15:39:49 +0000

    Experimented a bit with exercise 5

--- Aula 2/p5a.c
+++ Aula 2/p5a.c
@@ -1,14 +1,16 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int main(void)
 {
     int fd;
     char *text1="AAAAA";
-    char *text2="BBBBB";
-    fd = open("f1.txt",O_CREAT|O_EXCL|O_TRUNC|O_WRONLY|O_SYNC,0600);
+    char *text2="BBBBB\n";
+    fd = open("f1.txt",O_CREAT|O_WRONLY|O_SYNC|O_APPEND,0600);
+    getchar();
     write(fd,text1,5);
-    write(fd,text2,5);
+    write(fd,text2,6);
     close(fd);
     return 0;
 }
--- Aula 2/p5b.c
+++ Aula 2/p5b.c
@@ -1,14 +1,15 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int main(void)
 {
     int fd;
     char *text1="CCCCC";
-    char *text2="DDDDD";
-    fd = open("f1.txt", O_WRONLY|O_SYNC,0600);
+    char *text2="DDDDD\n";
+    fd = open("f1.txt", O_WRONLY|O_APPEND,0600);
     write(fd,text1,5);
-    write(fd,text2,5);
+    write(fd,text2,6);
     close(fd);
     return 0;
 }
