1c739758c820e0ee53baed75246619e1722cc516
MechJM <matosjoaops@protonmail.com>
2020-02-28 22:29:50 +0000

    exec_time is fixed

--- Aula 1/exec_time.c
+++ Aula 1/exec_time.c
@@ -11,7 +11,7 @@ int main(int argc,char *argv[])
     clock_t start,end;
     
     start=times(&sys_time);
-    long int ticks_sec = sysconf(_SC_CLK_TCK);
+    double ticks_sec = sysconf(_SC_CLK_TCK);
 
     int num1,num2,rand_num,i=1;
 
@@ -19,21 +19,18 @@ int main(int argc,char *argv[])
     num2=atoi(argv[2]);
 
     srand(time(NULL));
+
+    do 
+    {
     rand_num=rand()%num1;
     printf("%d:%d\n",i,rand_num);
-
-    while(rand_num!=num2)
-    {
-        i++;
-        rand_num=rand()%num1;
-        printf("%d:%d\n",i,rand_num);
-        sleep(1);
-    }
+    i++;
+    } while (rand_num != num2);
 
     
     end=times(&sys_time);
 
-    printf("Tempo real: %ld segundos\n",(end-start)/ticks_sec);
+    printf("Tempo real: %f segundos\n",(end-start)/ticks_sec);
     printf("Tempo CPU(user): %f segundos\n",(double)sys_time.tms_utime/ticks_sec);
     printf("Tempo CPU(system): %f segundos\n",(double)sys_time.tms_stime/ticks_sec);
 
