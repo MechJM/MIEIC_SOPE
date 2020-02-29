c162364d116dfeb6291a73ab5a65610f34996377
MechJM <matosjoaops@protonmail.com>
2020-02-29 16:29:29 +0000

    Ex. 6 and, as a result, class 2 exercises are done

--- Aula 2/p6a.c
+++ Aula 2/p6a.c
@@ -4,6 +4,7 @@
 #include <dirent.h> 
 #include <sys/stat.h> 
 #include <errno.h> 
+#include <unistd.h>
 
 int main(int argc, char *argv[]) 
 {  
@@ -24,15 +25,19 @@ int main(int argc, char *argv[])
         exit(2);  
     }  
     
+    link("temp.txt","temp1.txt");
+    symlink("temp.txt","temp2.txt");
+
     while ((direntp = readdir( dirp)) != NULL)  
     {   
-        lstat(direntp->d_name, &stat_buf);   
+        //lstat(direntp->d_name, &stat_buf);   
+        stat(direntp->d_name, &stat_buf);
         if (S_ISREG(stat_buf.st_mode)) str = "regular";
         else if (S_ISDIR(stat_buf.st_mode)) str = "directory";   
-        else str = "other";   
+        else str = "other"; 
+        printf("%-10ld  ",stat_buf.st_ino);  
         if (strcmp(str,"regular") !=0) printf("%-25s - %s\n", direntp->d_name, str);  
         else printf("%-25s - %-10s %ld\n",direntp->d_name,str,stat_buf.st_size);
-        printf("Inode number: %ld\n",stat_buf.st_ino);
     }  
     
     closedir(dirp);  
