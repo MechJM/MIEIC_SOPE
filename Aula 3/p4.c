9a94c5ef821bf76f267f0c3eedba8f3a393c0bf4
MechJM <matosjoaops@protonmail.com>
2020-03-02 16:51:32 +0000

    Ex.4 is done

---
+++ Aula 3/p4.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <sys/types.h>
+
+int main (void)
+{
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid == 0)
+    {  write(STDOUT_FILENO,"world!\n",7);
+        
+    }
+    else
+    {
+       write(STDOUT_FILENO,"Hello ",6);
+       
+    }
+    
+    return 0;
+}
