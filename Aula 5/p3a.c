7e3d8586ba3d6d62e49ae17dc6db365a15740a93
MechJM <matosjoaops@protonmail.com>
2020-03-23 23:59:02 +0000

    Ex.3 a) is semi functional

---
+++ Aula 5/p3a.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define READ  0
+#define WRITE 1
+#define MAXLEN 1000
+
+int main(int argc, char* argv[])
+{
+    int fd[2];
+    if (pipe(fd) < 0) fprintf(stderr,"Couldn't create pipe.\n");
+
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        close(fd[WRITE]);
+        dup2(fd[READ],STDIN_FILENO);
+        execvp("sort",&argv[1]);
+    }
+    else if (pid > 0)
+    {
+        
+        close(fd[READ]);
+        char content[MAXLEN];
+
+        int filedes;
+                
+        if ((filedes = open(argv[1],O_RDONLY)) < 0) fprintf(stderr,"Couldn't open file.\n");
+
+        read(filedes,content,MAXLEN);
+
+        write(fd[WRITE],content,sizeof(content));
+
+        close(fd[WRITE]);
+    }
+    else fprintf(stderr,"Couldn't create child process.\n");
+
+    return 0;
+}
